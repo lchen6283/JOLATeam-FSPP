55557463ef9ced8aca68cef0ccb7f5908dbca823
arupay <arupay11374@gmail.com>
2022-11-18 12:03:58 -0500

    edied tables for backend scheme and seed

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -3,17 +3,38 @@ CREATE DATABASE dev_smak;
 
 \c dev_smak;
 
-CREATE TABLE customer (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+CREATE TABLE users (
+    id SERIAL NOT NULL PRIMARY KEY,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
     address TEXT,
-    phonenumber CHAR(10)
+    city TEXT,
+    state TEXT,
+    zip TEXT,
+    phonenumber CHAR(10),
+    username TEXT NOT NULL,
+    token TEXT,
+    gary_points INT,
+    avatar TEXT,
+    is_active BOOLEAN
 );
 
-CREATE TABLE order_history (
+CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
+    restaurant_ID TEXT,
     date DATE,
-    restaurant_id TEXT,
-    customer_id INTEGER REFERENCES customer (id)
+    delivery_address TEXT,
+    total_cost INT,
+    userid INTEGER REFERENCES users (id)
     ON DELETE CASCADE
 );
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    userid INTEGER REFERENCES users (id),
+    orderid INTEGER REFERENCES orders (id),
+    rating smallint not null check (rating between 1 and 5),
+    content TEXT NOT NULL,
+    img_url TEXT,
+    date DATE
+)
