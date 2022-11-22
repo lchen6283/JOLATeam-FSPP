e8d8671c4eca1137b7efe82c1c924bbf4b728d33
arupay <arupay11374@gmail.com>
2022-11-22 17:37:53 -0500

    fixed errors in merging

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
   let navigate = useNavigate();
 
   const handleClick = () => {
-    navigate(`/products`);
+    navigate(`/about`);
   };
 
   return (
@@ -24,19 +24,20 @@ export default function Home() {
           </span>
         </h2>
         <p className="text-xl mt-4 max-w-md mx-auto text-gray-400">
-          Welcome to the world's most extensize and advanced E-commerce
-          platform.
+          Choose a location to get started.
         </p>
         <div className="lg:mt-0 lg:flex-shrink-0">
-          <div className="mt-12 inline-flex rounded-md shadow">
-            <button
-              onClick={() => handleClick()}
-              type="button"
-              className="py-3 px-6 bg-gray-600 hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-gray-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-            >
-              <Search setCity={setCity} />
-            </button>
-          </div>
+          <div className="mt-12 inline-flex rounded-md shadow"></div>
+          <Search setCity={setCity} city={city} />
+        </div>
+        <div className="mt-12 inline-flex rounded-md shadow">
+          <button
+            onClick={handleClick}
+            type="button"
+            className="py-3 px-6 bg-orange-600 hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-gray-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+          >
+            START
+          </button>
         </div>
       </div>
     </div>
