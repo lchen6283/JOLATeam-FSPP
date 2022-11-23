f7c30f441f3c969757d18890724371c12aa3aa4e
arupay <arupay11374@gmail.com>
2022-11-23 14:39:37 -0500

    all changes

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 import Search from "../Components/Search";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_URL;
 
 export default function Home() {
   const [city, setCity] = useState("Rego Park");
+  const [restaurants, setRestaurants] = useState([]);
   let navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate(`/about`);
+  console.log(restaurants);
+  const handleClick = async () => {
+    await axios
+      .get(`${API}/yelp`)
+      .then((res) => {
+        console.log(res);
+        setRestaurants(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
