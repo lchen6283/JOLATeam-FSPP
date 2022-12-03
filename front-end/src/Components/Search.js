78ef62fd43122e06382884b69b2229dbaefde844
Oscar  Andres  Varon <anvaron@gmail.com>
2022-12-02 19:28:31 -0500

    Update Search.js

--- front-end/src/Components/Search.js
+++ front-end/src/Components/Search.js
@@ -7,7 +7,7 @@ export default function Search({ city, setCity, handleClick }) {
 
   return (
     <div className="w-full p-2 px-10 md:mt-0 lg:flex-shrink-0  bg-smakHighlight">
-       <div className="p-3 md:mt-0 inline-flex rounded-md shadow top-17 left-1/2 -translate-x-1/6 -translate-y-1/6 font-['Fraunces']">
+       <div className="p-3 md:mt-0 inline-flex rounded-md shadow top-17 left-1/2 -translate-x-1/6 -translate-y-1/6 ">
         <span className="p-7 items-baseline text-white text-4xl md:text-6xl -translate-y-10">
           LET'S FIND A NEW<b className="text-smakorange"> FLAVOR!</b>
        
@@ -31,7 +31,7 @@ export default function Search({ city, setCity, handleClick }) {
             <button
               onClick={handleClick}
               type="button"
-              className="py-4 px-8 bg-smakorange hover:bg-smakHighlight focus:ring-gray-500 focus:ring-offset-gray-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg font-['Fraunces']"
+              className="py-4 px-8 bg-smakorange hover:bg-smakHighlight focus:ring-gray-500 focus:ring-offset-gray-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
             >
               START
             </button>
