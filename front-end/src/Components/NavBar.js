c5984a242e4b089d1997c01d7963848ebafa8597
lchen6283 <lchen6283@gmail.com>
2022-12-05 11:44:21 -0500

    about page edits

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -8,10 +8,8 @@ export default function NavBar() {
       <nav className="py-6 px-4 sticky top-0 z-50 border-gray-200  lg:px-6 py-2.5 ">
         <div className="flex flex-wrap justify-between py-2 items-center mx-auto max-w-screen-xl">
           <a href="/" className="flex items-center">
-
             <img src={logo} className="mr-3 h-6 sm:h-14" alt="Logo" />
             <span class="self-center text-4xl font-semibold whitespace-nowrap dark:text-white font-['Fraunces']">
-
               SMAK
             </span>
           </a>
@@ -19,7 +17,6 @@ export default function NavBar() {
             <a
               href="/login"
               class="text-smakorange dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800 font-['Fraunces']"
-
             >
               Sign in
             </a>
@@ -32,9 +29,7 @@ export default function NavBar() {
             <button
               data-collapse-toggle="mobile-menu-2"
               type="button"
-
               class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 font-['Fraunces']"
-
               aria-controls="mobile-menu-2"
               aria-expanded="false"
             >
@@ -83,9 +78,7 @@ export default function NavBar() {
                 {/* <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Company</a> */}
                 <Link
                   to="/orders"
-
                   className="block py-2 pr-4 pl-3 text-smakorange border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-smakHighlight lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700 font-['Fraunces']"
-
                 >
                   Orders
                 </Link>
@@ -99,15 +92,14 @@ export default function NavBar() {
                   About
                 </Link>
               </li>
-              <li>
-                {/* <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Features</a> */}
+              {/* <li>
                 <Link
                   to="/contact"
                   className="block py-2 pr-4 pl-3 text-smakorange border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-smakHighlight lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700 font-['Fraunces']"
                 >
-                  Contact
+                  Team
                 </Link>
-              </li>
+              </li> */}
             </ul>
           </div>
         </div>
