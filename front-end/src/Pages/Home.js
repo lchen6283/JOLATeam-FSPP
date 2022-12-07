e8d726303a6589b243ca0ac85efe2e688853e1db
Oscar  Andres  Varon <anvaron@gmail.com>
2022-12-07 10:29:03 -0500

    Update Home.js

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -349,61 +349,15 @@ export default function Home() {
         <h2 className="py-10 text-4xl text-gray-600 font-extrabold font-[Open Sans] ">
           <b className="text-6xl leading-8">What</b> our customers are saying
         </h2>
-        {reviews.length > 0 ? (
+        
           <div
             id="reviews-carousel"
             className="relative flowbite"
             data-carousel="static"
           >
             <div className="relative h-96 overflow-hidden rounded-0 ">
-              {reviews.map((review, i) => {
-                return (
-                  <div
-                    key={i}
-                    className="hidden duration-1000 ease-in-out"
-                    data-carousel-item
-                  >
-                    <section className="bg-white dark:bg-gray-900">
-                      <div className="max-w-screen-xl px-4 pt-8 pb-4 mx-auto text-center lg:px-6">
-                        <figure className="max-w-screen-md mx-auto">
-                          <svg
-                            className="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600"
-                            viewBox="0 0 24 27"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
-                              fill="currentColor"
-                            />
-                          </svg>
-                          <blockquote>
-                            <p className="text-2xl font-medium text-gray-900 dark:text-white">
-                              {review.content}
-                            </p>
-                          </blockquote>
-                          <figcaption className="flex items-center justify-center mt-6 space-x-3">
-                            <img
-                              className="w-6 h-6 rounded-full"
-                              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png"
-                              alt="profile picture"
-                            />
-                            <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
-                              <div className="pr-3 font-medium text-gray-900 dark:text-white">
-                                {review.firstName} {review.lastName}
-                              </div>
-                              <div className="pl-3 text-md font-md text-gray-600 dark:text-gray-400">
-                                Order Rate:{" "}
-                                <b className="font-bold">{review.rating}</b>
-                              </div>
-                            </div>
-                          </figcaption>
-                        </figure>
-                      </div>
-                    </section>
-                  </div>
-                );
-              })}
+              <div className="duration-1000 ease-in-out absolute inset-0 transition-all transform z-20 translate-x-0" data-carousel-item="true"><section className="bg-white dark:bg-gray-900"><div className="max-w-screen-xl px-4 pt-8 pb-4 mx-auto text-center lg:px-6"><figure className="max-w-screen-md mx-auto"><svg className="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"></path></svg><blockquote><p className="text-2xl font-medium text-gray-900 dark:text-white">Those enchiladas hit the spot so good, how did they know?  Im just blown away</p></blockquote><figcaption className="flex items-center justify-center mt-6 space-x-3"><img className="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture"><div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700"><div className="pr-3 font-medium text-gray-900 dark:text-white"> </div><div class="pl-3 text-md font-md text-gray-600 dark:text-gray-400">Order Rate: <b class="font-bold">5</b></div></div></figcaption></figure></div></section></div>
+              <div className="duration-1000 ease-in-out absolute inset-0 transition-all transform z-10 translate-x-full" data-carousel-item="true"><section className="bg-white dark:bg-gray-900"><div className="max-w-screen-xl px-4 pt-8 pb-4 mx-auto text-center lg:px-6"><figure className="max-w-screen-md mx-auto"><svg className="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"></path></svg><blockquote><p className="text-2xl font-medium text-gray-900 dark:text-white">These tongue tacos are the best i never had. Wow wow wow</p></blockquote><figcaption className="flex items-center justify-center mt-6 space-x-3"><img className="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture"><div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700"><div className="pr-3 font-medium text-gray-900 dark:text-white"> </div><div class="pl-3 text-md font-md text-gray-600 dark:text-gray-400">Order Rate: <b class="font-bold">5</b></div></div></figcaption></figure></div></section></div>
             </div>
             <div className="grid grid-cols-2 ">
               <div className="relative ">
@@ -460,11 +414,11 @@ export default function Home() {
               </div>
             </div>
           </div>
-        ) : (
-          <>
-            <p>Loading...</p>
-          </>
-        )}
+//         ) : (
+//           <>
+//             <p>Loading...</p>
+//           </>
+//         )}
       </section>
     </div>
   );
