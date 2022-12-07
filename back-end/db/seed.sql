04da95607a9803ebb7fbf09c4939b9241bea686d
arupay <arupay11374@gmail.com>
2022-12-07 08:31:26 -0500

    post order complete

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -8,14 +8,14 @@ VALUES
   'Andres','Varon','777 Broadway Av Suite 7','New York','New York','11372',9179993311,'anvaron@gmail.com','051801',0,null,true,1
 ),
 (
-  'Veronica','Perez','231 Gary Street','Elmhurst','New York','11372',5164729482,'veroperez@gmail.com','123456',0,null,true,2
+  'Piper','Williams','529 Broadway APT2F','New York','New York','10012',5164729482,'pWilliam0735@gmail.com','123456',0, 'https://fakeface.rest/face/view/55?gender=female&minimum_age=20&maximum_age=30' , true, 2
 ),
 (
   'Mike','Perez','237 Gary Street','Elmhurst','New York','11372',5162229482,'mikeperez@gmail.com','429njs2k',0,null,true,2
 );
 
 
-INSERT INTO orders (restaurant_id, date, delivery_address, total_cost, order_contents, userid) VALUES ('CaboRVC', '2022-11-11', '231 Gary Street Elmhurst New York 11372', 7500, null, 1),('CaboRVC', '2022-11-12', '231 Gary Street Elmhurst New York 11372', 10000, null, 2), ('PioPio', '2022-11-20', '200 West 44th Street, Hells Kitchen New York 10010', 100, null, 1);
+INSERT INTO orders (restaurant_id, restaurant_name, date, delivery_address, total_cost, order_contents, userid) VALUES (null, 'CaboRVC', '2022-11-11', '231 Gary Street Elmhurst New York 11372', 7500, null, 1),(null, 'CaboRVC', '2022-11-12', '231 Gary Street Elmhurst New York 11372', 10000, null, 2), (null, 'PioPio', '2022-11-20', '200 West 44th Street, Hells Kitchen New York 10010', 100, null, 1);
 
 INSERT INTO reviews (userid, orderid, rating, content, img_url, date) VALUES (1, 1, 5, 'Those enchiladas hit the spot so good, how did they know?  Im just blown away', 'https://bloximages.chicago2.vip.townnews.com/tucson.com/content/tncms/assets/v3/editorial/3/de/3de25464-fc61-11ea-bad1-4f7f5411be85/5f6935d287a6c.image.jpg', '2022-11-11'), (2, 2, 5, 'These tongue tacos are the best i never had. Wow wow wow', 'https://bloximages.chicago2.vip.townnews.com/tucson.com/content/tncms/assets/v3/editorial/3/de/3de25464-fc61-11ea-bad1-4f7f5411be85/5f6935d287a6c.image.jpg', '2022-11-12'),
 (1,3,5, 'First time trying Peruvian food and I cannot wait to go back.', 'https://gradfood.com/wp-content/uploads/2022/04/Lomo-Saltado-2-scaled.jpg','2022-11-20');
